9990a8192579eabff361d4107610a102255a11e4
Ryan Parrish <ryan@stickystyle.net>
2015-04-08 08:58:45 -0400

    making the timeout 1 second

--- aptproxy.sh
+++ aptproxy.sh
@@ -5,7 +5,7 @@
 # avahi dameon is not running (like in a Docker container) so the proxy server
 # can be discovered dynamicly without any values being hardcoded
 
-dig +nocmd +noall +answer @224.0.0.251 -p 5353 -t ptr _apt_proxy._tcp.local > /tmp/aptproxy
+dig +nocmd +noall +answer +time=1 @224.0.0.251 -p 5353 -t ptr _apt_proxy._tcp.local > /tmp/aptproxy
 APTPROXY_HOST=$(grep "IN\sA\s" /tmp/aptproxy | awk '{print $5}')
 APTPROXY_PORT=$(grep "IN\sSRV" /tmp/aptproxy | awk '{print $7}')
 echo "http://$APTPROXY_HOST:$APTPROXY_PORT/"
